c0e7d535e5f72a787a92ebb1a047724e7a32751b
Sergio Hess <hess.sergio@gmail.com>
2026-03-09 04:11:48 -0300

    feat: implementar função para verificar par de números com soma alvo

--- desafio_01/solution.py
+++ desafio_01/solution.py
@@ -1,3 +1,10 @@
 def has_pair_with_sum(nums: list[int], target: int) -> bool:
     """Verifica se existe um par de números cuja soma é target."""
-    raise NotImplementedError
+    for index_main, item_main in enumerate(nums):
+        for index_sec, item_sec in enumerate(nums):
+            if index_main == index_sec:
+                continue
+            if item_main + item_sec == target:
+                return True
+
+    return False
