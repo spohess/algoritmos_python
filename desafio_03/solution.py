ffc7eb5fb1303dc4127db8c1464ee9060c64945e
Sergio Hess <hess.sergio@gmail.com>
2026-03-15 16:47:38 -0300

    feat: adicionar pontuação e justificativas para o desafio 03

--- desafio_03/solution.py
+++ desafio_03/solution.py
@@ -1,5 +1,12 @@
 def remove_duplicates(nums: list[int]) -> list[int]:
     result = []
     for num in nums:
+        find = False
         for selected in result:
-            if selected
+            if num == selected:
+                find = True
+                break
+        if not find:
+            result.append(num)
+    
+    return result
